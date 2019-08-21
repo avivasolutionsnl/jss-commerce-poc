81522502fc4b06958e0ef2587759a14f0f8fa18a
Jonne Kats <jonne.kats@avivasolutions.nl>
2019-08-21 16:24:05 +0200

    Add price

--- app/src/components/Price/index.js
+++ app/src/components/Price/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import fetch from 'node-fetch';
+import React, {useContext, useEffect, useState} from 'react';
 import { withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { gatewayUrl } from '../../temp/config';
 
-const Price = (props) => (
-  <div className='product-price'>
-    <div className='currency'>
-      {props.fields.listprice.currency}
-    </div>  
-    <div className='price'>
-      {props.fields.listprice.value}
+const Price = (props) => {
+
+  const catalogId = 'Habitat_Master';
+  const productId = props.sitecoreContext.route.fields.ProductId.value;
+
+  const [productData, setProductData] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      fetch(`${gatewayUrl}/api/catalog/${catalogId}/sellableitems/${productId}`)
+        .then(res => res.json())
+        .then(data => setProductData(data))
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='product-price'>
+      <span className='currency'>
+        {productData && productData.ListPrice && productData.ListPrice.CurrencyCode}
+      </span>  
+      <span className='price'>
+        {productData && productData.ListPrice && productData.ListPrice.Amount}
+      </span>
     </div>
-  </div>
-);
+  );
+};
 
 export default withSitecoreContext()(Price);
