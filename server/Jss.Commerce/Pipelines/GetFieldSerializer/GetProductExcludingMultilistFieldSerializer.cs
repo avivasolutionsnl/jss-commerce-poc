8a220b8665d161a29b73e6bf886dfb349d5d4d85
Jonne Kats <jonne.kats@avivasolutions.nl>
2019-08-22 11:36:26 +0200

    Fix recursive loop by preventing product relation serialization

---
+++ server/Jss.Commerce/Pipelines/GetFieldSerializer/GetProductExcludingMultilistFieldSerializer.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Serialization;
+using Sitecore.LayoutService.Serialization.Pipelines.GetFieldSerializer;
+
+namespace Jss.Commerce.Pipelines.GetFieldSerializer
+{
+    public class GetProductExcludingMultilistFieldSerializer : BaseGetFieldSerializer
+    {
+        public GetProductExcludingMultilistFieldSerializer(IFieldRenderer fieldRenderer)
+          : base(fieldRenderer)
+        {
+        }
+
+        protected override void SetResult(GetFieldSerializerPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, nameof(args));
+            Assert.IsNotNull(args.Field, "args.Field is null");
+            Assert.IsNotNull(args.ItemSerializer, "args.ItemSerializer is null");
+            args.Result = new ProductExcludingMultilistFieldSerializer(args.ItemSerializer, FieldRenderer);
+        }
+    }
+}
---
+++ server/Jss.Commerce/Pipelines/GetFieldSerializer/ProductExcludingMultilistFieldSerializer.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using Sitecore.Commerce;
+using Sitecore.Commerce.Products;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.LayoutService.Serialization;
+using Sitecore.LayoutService.Serialization.FieldSerializers;
+using Sitecore.LayoutService.Serialization.ItemSerializers;
+
+namespace Jss.Commerce.Pipelines.GetFieldSerializer
+{
+    /// <summary>
+    /// Custom version the MultilistFieldSerializer that prevents product relations from getting
+    /// serialized. With the default MultilistFieldSerializer, the entire site will crash because it tries
+    /// to serialize all recursive product relations.
+    /// </summary>
+    public class ProductExcludingMultilistFieldSerializer : BaseFieldSerializer
+    {
+        private readonly ID productTemplateId = new ID("{225F8638-2611-4841-9B89-19A5440A1DA1}");
+        protected readonly IItemSerializer ItemSerializer;
+
+        public ProductExcludingMultilistFieldSerializer(IItemSerializer itemSerializer, IFieldRenderer fieldRenderer)
+          : base(fieldRenderer)
+        {
+            Assert.ArgumentNotNull(itemSerializer, nameof(itemSerializer));
+            ItemSerializer = itemSerializer;
+        }
+
+        public override void Serialize(Field field, JsonTextWriter writer)
+        {
+            Assert.ArgumentNotNull(field, nameof(field));
+            Assert.ArgumentNotNull(writer, nameof(writer));
+
+            Log.Info("Serializing multi list field", this);
+
+            using (RecursionLimit recursionLimit = new RecursionLimit(string.Format("{0}|{1}|{2}", GetType().FullName, field.Item.ID, field.ID), 1))
+            {
+                if (recursionLimit.Exceeded)
+                {
+                    return;
+                }
+
+                Item[] items = ((MultilistField)field).GetItems();
+                if (items == null || items.Length == 0)
+                {
+                    writer.WritePropertyName(field.Name);
+                    writer.WriteStartArray();
+                    writer.WriteEndArray();
+                }
+                else
+                {
+                    writer.WritePropertyName(field.Name);
+                    writer.WriteStartArray();
+                    foreach (Item obj in items)
+                    {
+                        if (obj.IsDerived(productTemplateId))
+                        {
+                            Log.Info($"Not serializing multi list field item: {obj.ID}, because its a product", this);
+                            continue;
+                        }
+
+                        Log.Info($"Serializing multi list field item: {obj.ID} not a product", this);
+
+                        writer.WriteStartObject();
+                        writer.WritePropertyName("id");
+                        writer.WriteValue(obj.ID.Guid.ToString());
+                        writer.WritePropertyName("fields");
+                        writer.WriteRawValue(ItemSerializer.Serialize(obj));
+                        writer.WriteEndObject();
+                    }
+                    writer.WriteEndArray();
+                }
+            }
+        }
+    }
+}
