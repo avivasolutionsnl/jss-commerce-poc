ef84179b545c407b55133ccb65db2565871733d8
Joost Meijles <joost@meijles.com>
2019-08-29 13:17:39 +0200

    Fix Order component

--- app/src/components/Order/index.js
+++ app/src/components/Order/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Text } from '@sitecore-jss/sitecore-jss-react';
 
 const Order = (props) => (
   <div>
     <p>Thank you for ordering!</p>
-    <Text field={props.fields.heading} />
   </div>
 );
 
