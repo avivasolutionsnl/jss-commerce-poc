8619c2bdbffd7be887de905af818ac4a5a4432cd
Joost Meijles <joost@meijles.com>
2019-09-09 10:37:01 +0200

    Add key to ProductList

--- app/src/shared/components/productlist.js
+++ app/src/shared/components/productlist.js
@@ -3,7 +3,7 @@ import ProductSummary from './productsummary';
 
 const ProductList = ({products}) => {
     return <div className='productlist'>
-        {products && products.map(product => <ProductSummary {...product} />)}
+        {products && products.map((product, i) => <ProductSummary key={i} {...product} />)}
     </div>
 }
 
